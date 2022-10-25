f3d66691a50564e81b25b3457329fdeba91faac9
sazzadhossainapple <sazzadapple81@gmail.com>
2022-10-25 20:01:54 +0600

    data added

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require("./data/Courses.json");
+
 app.use(cors());
 app.get("/", (req, res) => {
   res.send(" Programming learning reactive server is running");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const singleCourse = courses.find((course) => course.id === id);
+  res.send(singleCourse);
 });
 
 app.listen(port, () => {
